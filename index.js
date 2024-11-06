489221ce1515c9fce7c1d4c89f8806c35e652582
Papa Issa D. <papaissad@github.com>
2024-11-06 16:44:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,7 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
-  // create an issue to greet the user and use the github-token input to authenticate
-  const octokit = github.getOctokit(core.getInput("github-token"));
-  octokit.rest.issues.create({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    title: `Hello, ${nameToGreet}!`,
-    body: `This is an automated issue created by the Say Hello Action on ${new Date().toDateString()}.`,
-  });
+
 
 } catch (error) {
   core.setFailed(error.message);
